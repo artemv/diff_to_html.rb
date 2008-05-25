2120fa0205df356a7201a33bc7b5fcbde5cd1641
unknown <abublic@gmail.com>
2008-05-25 16:43:27 +0400

    let's use svn in example

--- examples/test.rb
+++ examples/test.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 gem 'artemv-diff_to_html'
 require 'diff_to_html'
 #require '../lib/diff_to_html.rb'
-#diff = `svn diff -r 46:47 svn://hamptoncatlin.com/haml --diff-cmd diff -x "-U 2"`
-diff = `cat diff`
+diff = `svn diff -r 46:47 svn://hamptoncatlin.com/haml --diff-cmd diff -x "-U 2"`
+#diff = `cat diff`
 puts <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
