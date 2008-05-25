2a88c4221ce11b34f9fba5f73044f267149dfd44
unknown <abublic@gmail.com>
2008-05-25 16:39:30 +0400

    let's output unicode html

--- examples/test.rb
+++ examples/test.rb
@@ -2,17 +2,18 @@ require 'rubygems'
 gem 'artemv-diff_to_html'
 require 'diff_to_html'
 #require '../lib/diff_to_html.rb'
-diff = `svn diff -r 46:47 svn://hamptoncatlin.com/haml --diff-cmd diff -x "-U 2"`
-#diff = `cat diff`
+#diff = `svn diff -r 46:47 svn://hamptoncatlin.com/haml --diff-cmd diff -x "-U 2"`
+diff = `cat diff`
 puts <<EOF
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-      <link type="text/css" rel="stylesheet" href="diff.css">
-      <title>diff of HAML revision 47</title>
-    </head>
-    <body>
-    #{DiffToHtml.new.get_diff(diff)}
-    </body>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+  <link type="text/css" rel="stylesheet" href="diff.css">
+  <title>diff of HAML revision 47</title>
+</head>
+<body>
+#{DiffToHtml.new.get_diff(diff)}
+</body>
 </html>
 EOF
